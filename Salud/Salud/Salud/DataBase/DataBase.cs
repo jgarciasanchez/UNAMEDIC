c8d65047dc32d4faadcdf88fcd974aa22756ddd1
LAPTOP-RPKJ3IRH\tvkev <tvkevin@gmail.com>
2020-11-14 14:51:11 -0600

    CRUD SQLite

--- Salud/Salud/Salud/DataBase/DataBase.cs
+++ Salud/Salud/Salud/DataBase/DataBase.cs
@@ -1,10 +1,122 @@
-﻿using System;
+﻿using Salud.Models;
+using SQLite;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Salud.DataBase
 {
-    class DataBase
+    public class DataBase
     {
+        public string folder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+        public string database = "Salud";
+
+        public bool instanceDB()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, database)))
+                {
+                    var temp = System.IO.Path.Combine(folder, database);
+                    connection.CreateTable<Item>();
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                return false;
+            }
+        }
+        public bool rebuildDB()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, database)))
+                {
+                    connection.DropTable<Item>();
+                    connection.CreateTable<Item>();
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                return false;
+            }
+        }
+
+        #region Tabla Ejemplo Item
+        public bool saveItem(Item item)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, database)))
+                {
+                    connection.Insert(item);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                return false;
+            }
+        }
+        public bool deleteItem(Item item)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, database)))
+                {
+                    connection.Delete(item);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                return false;
+            }
+        }
+        public bool updateItem(Item item)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, database)))
+                {
+                    connection.Update(item);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                return false;
+            }
+        }
+        public List<Item> getItems()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, database)))
+                {
+                    return connection.Table<Item>().ToList();
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                //Log.Info("SQLiteEx", ex.Message);
+                return null;
+            }
+        }
+        public Item getItemByID(int Id)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, database)))
+                {
+                    return connection.Query<Item>("SELECT * FROM Item Where ID=?", Id)[0];
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                return null;
+            }
+        }
+        #endregion
     }
 }
--- Salud/Salud/Salud/Models/Item.cs
+++ Salud/Salud/Salud/Models/Item.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 
 namespace Salud.Models
 {
+
+    [Table("Item")]
     public class Item
     {
         public string Id { get; set; }
