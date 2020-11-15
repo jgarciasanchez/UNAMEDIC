7ce904a83a9a4637d2e96c7203cd9a8910a5f164
LAPTOP-RPKJ3IRH\tvkev <tvkevin@gmail.com>
2020-11-15 16:27:17 -0600

    Update HipertensionViewModel.cs

--- Salud/Salud/Salud/ViewModels/HipertensionViewModel.cs
+++ Salud/Salud/Salud/ViewModels/HipertensionViewModel.cs
@@ -1,11 +1,59 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Salud.ViewModels
 {
     public class HipertensionViewModel : BaseViewModel
     {
+        #region Attributes //Aquí van los propieades que serán modificadas en el controlador 
+        //Atributos de LoginPage
+        private string _edtxUsuario;
+        private string _edtxClave;
 
+        //Controller
+
+        #endregion
+
+        #region Properties //Aquí van los elementos desde la vista bindeados
+        public string edtxUsuario
+        {
+            get { return this._edtxUsuario; }
+            set { this.SetValue(ref this._edtxUsuario, value); } // no solo asigna, también refresca la vista...
+        }
+        public string edtxClave
+        {
+            get { return this._edtxClave; }
+            set { this.SetValue(ref this._edtxClave, value); } // no solo asigna, también refresca la vista...
+        }
+
+        #endregion
+
+        #region  Constructors
+        public HipertensionViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Commands
+        public ICommand OnMenuItemConfiguracionesClicked
+        {
+            get
+            {
+                return new RelayCommand(OnMenuItemConfiguraciones);
+            }
+        }
+        #endregion
+
+        #region Methods
+        public async void OnMenuItemConfiguraciones()
+        {
+            //await Application.Current.MainPage.Navigation.PushModalAsync(new LoginPage()); //Para una pantalla modal
+            // await Shell.Current.GoToAsync(nameof(HipertensionTabbedPage)); //Para llamar otra vista
+        }
+        #endregion
     }
 }
